c8209c20fd81dde3a281bb47916b2facbde34738
ruiichen <frankchen99999@gmail.com>
2025-02-08 23:21:26 -0500

    NOJIRA: - fixed docker setup problems

--- hackers/app.py
+++ hackers/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_graphql import GraphQLView
-
+from hackers.database.init_db import init_db
 from .database.db_session import db_session
 from .schema.schema import schema
 
@@ -15,6 +15,10 @@ app.add_url_rule(
         graphiql=True
     )
 )
+
+@app.before_first_request
+def initialize_database():
+    init_db()
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from hackers.app import app
-from hackers.database.init_db import init_db
 
 def main():
-    init_db()
     app.run()
 
 if __name__ == '__main__':
